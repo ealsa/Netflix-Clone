07c4e84ee61b68c269d2e8de54fd090a2a7c65f3
Emre Alsancak <ec.alsancak@gmail.com>
2022-06-23 16:43:35 +0300

    First commit

---
+++ src/pages/Account.js
@@ -0,0 +1,23 @@
+import React from "react";
+import SavedShows from "../components/SavedShows";
+
+const Account = () => {
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/5ea364b1-8e59-4693-8ad8-f0eaee32d1bf/8521927a-abf7-498a-9bda-de3c0ac9e526/TR-tr-20220530-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
+};
+
+export default Account;
